83fd15f76041c3126ac4ca5a800a58b13a44d1d1
eumibiag@gmail.com <eumibiag@gmail.com>
2025-04-06 12:37:24 +0800

    Add greeting counter functionality with increment, decrement, and save features

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let increment = document.getElementById("greetingAdd");
+let decrement = document.getElementById('greetingSubtract')
+let save = document.getElementById('save');
+let greeters = document.getElementById('greeters');
+let count = 0;
+let total = document.getElementById('total')
+
+function addGreetings(){
+    count += 1;
+    greeters.textContent = count;
+}
+
+function subtractGreetings(){
+    count -= 1;
+    greeters.textContent = count;
+}
+
+function saveGreetings(){
+    saved = count  + " - ";
+    total.textContent += saved;
+    greeters.textContent = 0;
+}
